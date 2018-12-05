653f4bfc774f5c9591b508b78516715e4799eeb7
Pedro Arvellos <pedroarvellos@hotmail.com>
2018-12-05 13:35:21 -0200

    [MR-0003] Creation of the folder Structure

--- my-reads/src/App.js
+++ my-reads/src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import classes from './App.css';
+
+import Auxiliary from "./hoc/Auxiliary/Auxilary"
 
 class App extends Component {
   render() {
     return (
-      <div className={classes.App}>
-        <header className={classes.AppHeader}>
-          <img src={logo} className={classes.AppLogo} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={classes.AppLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Auxiliary>
+        <div>First Component</div>
+        <div>Second Component</div>
+      </Auxiliary>
     );
   }
 }
