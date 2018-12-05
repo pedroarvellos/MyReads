6faf67d75598c7d5a6f6236768041816a6c24a5d
Pedro Arvellos <pedroarvellos@hotmail.com>
2018-12-05 15:06:53 -0200

    [MR-0006] First CSS for Search component created and HTML implemented

--- my-reads/src/components/BookShelf/BookShelf.js
+++ my-reads/src/components/BookShelf/BookShelf.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import classes from './BookShelf.css'
 
 const bookShelf = (props) => (
-    <div>bookShelf Component</div>
+    <div className="classes.App">
+        <div className={classes.booksList}>
+            <div className={classes.BooksListTitle}>
+                <h1>MyReads</h1>
+            </div>
+            <div className={classes.BooksListContent}>
+                <div className={classes.Bookshelf}>
+                    <h2 className={classes.BookshelfTitle}>Currently Reading</h2>
+                    <div className={classes.BookshelfBooks}>
+                        <ol className={classes.BooksGrid}>
+                        </ol>
+                    </div>
+                </div>
+                <div className={classes.Bookshelf}>
+                    <h2 className={classes.BookshelfTitle}>Want to Reading</h2>
+                    <div className={classes.BookshelfBooks}>
+                        <ol className={classes.BooksGrid}>
+                        </ol>
+                    </div>
+                </div>
+                <div className={classes.Bookshelf}>
+                    <h2 className={classes.BookshelfTitle}>Read</h2>
+                    <div className={classes.BookshelfBooks}>
+                        <ol className={classes.BooksGrid}>
+                        </ol>
+                    </div>
+                </div>
+          </div>
+        </div>
+        <div className={classes.OpenSearch}>
+            <Link to="/search">Add a book</Link>
+        </div>
+    </div>
 );
 
 export default bookShelf;
--- my-reads/src/containers/Search/Search.js
+++ my-reads/src/containers/Search/Search.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import classes from "./Search.css"
 
 class Search extends Component {
     render() {
         return(
-            <div>Search Component</div>
+            <div className={classes.SearchBooks}>
+                <div className={classes.SearchBooksBar}>
+                <Link to="/" className={classes.CloseSearch}>
+                    Close
+                </Link>
+                <div className={classes.SearchBooksInputWrapper}>
+                    <input
+                        type="text"
+                        placeholder="Search by title or author"
+                    />
+                </div>
+                </div>
+                <div className={classes.SearchBooksResult}>
+                    <ol className={classes.BooksGrid}>
+
+                    </ol>
+                </div>
+            </div>
         );
     };
 }
