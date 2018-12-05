9954737002f8509953f8ca4ecdf022f65d42d015
Pedro Arvellos <pedroarvellos@hotmail.com>
2018-12-05 14:08:51 -0200

    [MR-0004] Redux Basic Configuration Implemented

--- my-reads/src/index.js
+++ my-reads/src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {  createStore, applyMiddleware, compose, combineReducers  } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import searchReducer from './store/reducers/searchReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const rootReduce = combineReducers({
+    searchReducer: searchReducer
+});
+
+const store = createStore(rootReduce, composeEnhancers(
+    applyMiddleware(thunk)
+));
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
+
 serviceWorker.unregister();
